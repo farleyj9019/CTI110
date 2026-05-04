65b5262f665a4af4baa1559e4f6bee43442f8d62
farleyj9019 <farleyj9019@student.faytechcc.edu>
2026-05-04 11:06:54 -0400

    P5LAB Module8
    P5LAB Module8

---
+++ P5Lab_FarleyJustin.py
@@ -0,0 +1,75 @@
+# Justin Farley
+# 5/4/2026
+# P5Lab_FarleyJustin
+# simulate a customer using a self-checkout machine, using user-defined functions
+
+import random
+
+def disperse_change(change):
+    # Converting the value to an integer
+    change = round(change * 100)
+
+    # Determine the number of dollars, quarters, dimes, nickels, and pennies in the change
+    num_dollars = change // 100
+    change = change - (num_dollars * 100)
+
+    num_quarters = change // 25
+    change = change - (num_quarters * 25)
+
+    num_dimes = change // 10
+    change = change - (num_dimes * 10)
+
+    num_nickels = change // 5
+    change = change - (num_nickels * 5)
+
+    num_pennies = change
+
+    if num_dollars > 0:
+        if num_dollars == 1:
+            print(f"{num_dollars} Dollar")
+        else:        
+            print(f"{num_dollars} Dollars")
+
+    if num_quarters > 0:
+        if num_quarters == 1:
+            print(f"{num_quarters} Quarter")
+        else:        
+            print(f"{num_quarters} Quarters")
+
+    if num_dimes > 0:
+        if num_dimes == 1:
+            print(f"{num_dimes} Dime")
+        else:        
+            print(f"{num_dimes} Dimes")
+
+    if num_nickels > 0:
+        if num_nickels == 1:
+            print(f"{num_nickels} Nickel")
+        else:        
+            print(f"{num_nickels} Nickels")
+
+    if num_pennies > 0:
+        if num_pennies == 1:
+            print(f"{num_pennies} Penny")
+        else:        
+            print(f"{num_pennies} Pennies")
+
+def main() :
+    #Logic goes here
+
+    #Gernerate the amount owed
+    amount_owed = round(random.uniform(0.01, 100.00), 2)
+    print(f"You owe: ${amount_owed:.2f}")
+
+    #Create variable to represent money put into machine
+    money_in = float(input("How much cash will you put in the self-checkout? "))
+
+    #Caculate change owed
+    change = money_in - amount_owed
+    print(f"Change owed: ${change:.2f}")
+
+    #Call the desperse change function
+    disperse_change(change)
+
+#Call the main function
+main()
